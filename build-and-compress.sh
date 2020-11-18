4e8c52aa7a87fa2d8e92bd053e03f609579aacd1
Sylvan Le Deunff <sylvan.ledeunff@orange.com>
2020-11-19 00:43:24 +0100

    use good old interface, but build for all platform because of buggy cli

--- build-and-compress.sh
+++
@@ -1,8 +0,0 @@
-$PROCESSING --sketch=$SKETCH/ --export && echo "Distribuables built for targeted platforms."
-
-mkdir -p ./artifacts
-
-for platform in $PLATFORMS
-do
-zip -r "artifacts/$SKETCH-$platform.zip" "space_invaders/application.$platform"
-done
